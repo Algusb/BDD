76c32520ff0f3c9bf3f9bdf483582d4b2525eedf
Aleks Gusev <algusb@mail.ru>
2022-11-03 19:54:22 +0300

    create test

--- src/test/java/ru/netology/web/page/LoginPage.java
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -1,9 +1,11 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
 
 public class LoginPage {
     private SelenideElement loginField = $("[data-test-id=login] input");
@@ -14,6 +16,6 @@ public class LoginPage {
         loginField.setValue(info.getLogin());
         passwordField.setValue(info.getPassword());
         loginButton.click();
-        return new VerificationPage();
+        return page(VerificationPage.class);
     }
 }
